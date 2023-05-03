5e738f7de46399cfee9ca9c94e2e46a69ca3168b
roushanali064 <roushanrhaman064@gmail.com>
2023-05-03 18:10:54 +0600

    chef image includ done

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors')
+const chefInfo = require('./data/chefInfo.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('DishDash Server runniang')
 });
+
+app.get('/chef',(req,res)=>{
+    res.send(chefInfo)
+})
 
 app.listen(port, () => {
     console.log('disdash running', port)
